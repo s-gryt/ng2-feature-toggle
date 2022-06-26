7562670cde265bc0b1512181352d8f7a28c26e24
Sergii <sergiigrytsaienko@gmail.com>
2022-06-26 18:06:01 -0500

    feat(feature toggle module): module update - exporting tokens and directive

--- projects/ng2-feature-toggle/src/lib/feature-toggle.module.ts
+++ projects/ng2-feature-toggle/src/lib/feature-toggle.module.ts
@@ -1,12 +1,48 @@
-import { NgModule } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { QUERY_PARAMS, queryParamsTokenFactory, FEATURES, FEATURE_QUERY_PARAM } from './providers';
+import { featureToggleFactory, FEATURE_TOGGLE } from './providers/feature-toggle.provider';
+import { Features } from './models';
+import { FeatureToggleDirective } from './directives/feature-toggle.directive';
+import { Inject, ModuleWithProviders, NgModule } from '@angular/core';
+import { InstanceInjector } from './shared';
 
-export * from './models';
-export * from './shared';
+export * from './directives';
 export * from './feature-toggle';
+export * from './query-params';
+export * from './models';
+export { FEATURE_TOGGLE } from './providers/feature-toggle.provider';
 
 @NgModule({
-  declarations: [],
-  imports: [],
-  exports: [],
+  declarations: [FeatureToggleDirective],
+  providers: [
+    {
+      provide: QUERY_PARAMS,
+      useFactory: queryParamsTokenFactory,
+      deps: [[new Inject(DOCUMENT)]],
+    },
+  ],
+  imports: [InstanceInjector],
+  exports: [FeatureToggleDirective],
 })
-export class FeatureToggleModule {}
+export class FeatureToggleModule {
+  static forRoot(featureQueryParamName: string, features: Features): ModuleWithProviders<FeatureToggleModule> {
+    return {
+      ngModule: FeatureToggleModule,
+      providers: [
+        {
+          provide: FEATURE_QUERY_PARAM,
+          useValue: featureQueryParamName,
+        },
+        {
+          provide: FEATURES,
+          useValue: features,
+        },
+        {
+          provide: FEATURE_TOGGLE,
+          useFactory: featureToggleFactory,
+          deps: [FEATURE_QUERY_PARAM, FEATURES],
+        },
+      ],
+    };
+  }
+}
