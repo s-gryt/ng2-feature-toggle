33c4de3c16f6cf4cfd7a84b608acdbe795d2b36a
Sergii <sergiigrytsaienko@gmail.com>
2022-06-26 18:00:25 -0500

    feat(feature toggle): feature toggle update - only feature needed to get the feature toggle config

--- projects/ng2-feature-toggle/src/lib/feature-toggle/feature-toggle.spec.ts
+++ projects/ng2-feature-toggle/src/lib/feature-toggle/feature-toggle.spec.ts
@@ -3,48 +3,62 @@ import {
   experimentFeatureName,
   features,
   invalidFeatureName,
+  queryParam,
   releaseFeatureName,
   searchQuery,
 } from '../mock';
 import { Feature, IFeature } from '../models';
 import { FeatureToggle } from './feature-toggle';
-import { IFeatureToggle } from '../models/feature-config.model';
+import { IFeatureToggle } from '../models/feature-toggle.model';
 import { Mode } from '../shared';
+import { QueryParams } from '../query-params';
 
 describe('FeatureToggle', () => {
-  const featureToggle = new FeatureToggle(features);
+  const featureToggle = new FeatureToggle(queryParam, features);
   const featureConfigMock = (feature: IFeature, isEnabled: boolean): IFeatureToggle => ({
     ...feature,
     isEnabled,
   });
+
+  let queryParamsSpy: jasmine.Spy<() => string>;
+  beforeEach(() => {
+    queryParamsSpy = spyOn(QueryParams.prototype, 'getSearchParams');
+  });
+
   describe(`#getFeatureToggle`, () => {
-    it('should return feature config', () => {
-      expect(featureToggle.getFeatureToggle(disalbeFeatureName, searchQuery)).toEqual(
-        featureConfigMock(features[disalbeFeatureName], true)
-      );
-      expect(featureToggle.getFeatureToggle(invalidFeatureName, searchQuery)).toEqual(
-        featureConfigMock(new Feature('' as unknown as Mode, invalidFeatureName, '', ''), false)
-      );
-    });
-    describe(`${Mode.Experiment}`, () => {
-      it('should return feature config', () => {
-        expect(featureToggle.getFeatureToggle(experimentFeatureName, searchQuery)).toEqual(
-          featureConfigMock(features[experimentFeatureName], true)
+    describe(`${Mode.Disable}`, () => {
+      it('should return feature toggle', () => {
+        queryParamsSpy.and.returnValue(searchQuery);
+        expect(featureToggle.getFeatureToggle(disalbeFeatureName)).toEqual(
+          featureConfigMock(features[disalbeFeatureName], true)
         );
-        expect(featureToggle.getFeatureToggle(experimentFeatureName, '')).toEqual(
-          featureConfigMock(features[experimentFeatureName], false)
-        );
-        expect(featureToggle.getFeatureToggle(invalidFeatureName, searchQuery)).toEqual(
+        expect(featureToggle.getFeatureToggle(invalidFeatureName)).toEqual(
           featureConfigMock(new Feature('' as unknown as Mode, invalidFeatureName, '', ''), false)
         );
       });
     });
+    describe(`${Mode.Experiment}`, () => {
+      it('should return feature toggle', () => {
+        queryParamsSpy.and.returnValue(searchQuery);
+        expect(featureToggle.getFeatureToggle(experimentFeatureName)).toEqual(
+          featureConfigMock(features[experimentFeatureName], true)
+        );
+        expect(featureToggle.getFeatureToggle(invalidFeatureName)).toEqual(
+          featureConfigMock(new Feature('' as unknown as Mode, invalidFeatureName, '', ''), false)
+        );
+        queryParamsSpy.and.returnValue('');
+        expect(featureToggle.getFeatureToggle(experimentFeatureName)).toEqual(
+          featureConfigMock(features[experimentFeatureName], false)
+        );
+      });
+    });
     describe(`${Mode.Relase}`, () => {
-      it('should return feature config', () => {
-        expect(featureToggle.getFeatureToggle(releaseFeatureName, searchQuery)).toEqual(
+      it('should return feature toggle', () => {
+        queryParamsSpy.and.returnValue(searchQuery);
+        expect(featureToggle.getFeatureToggle(releaseFeatureName)).toEqual(
           featureConfigMock(features[releaseFeatureName], true)
         );
-        expect(featureToggle.getFeatureToggle(invalidFeatureName, searchQuery)).toEqual(
+        expect(featureToggle.getFeatureToggle(invalidFeatureName)).toEqual(
           featureConfigMock(new Feature('' as unknown as Mode, invalidFeatureName, '', ''), false)
         );
       });
